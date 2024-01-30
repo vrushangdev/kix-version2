66067c11266a0b8d53ef92e036ad233c55bb09f1
noor alam <51040978+noorshanu@users.noreply.github.com>
2024-01-30 23:56:03 +0530

    Update ClickStart.jsx

--- src/components/ClickStart.jsx
+++ src/components/ClickStart.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function ClickStart() {
   return (
     <section className=" relative -mt-[5%] sm:-mt-[1%]">
-        <div className=" mx-auto  justify-center mt-5 mb-2  sm:mb-0 sm:mt-0 flex">
+        <div className=" mx-auto  justify-center mt-5 mb-0  sm:mb-0 sm:mt-0 flex">
             <a
               href="https://test.kix.digital/?utm_source=landingpage&utm_medium=website&utm_campaign=tradeathon&utm_content=wave3&utm_term=1millionkixtokens" 
               className=" uppercase archo-black text-lg sm:text-3xl txt-s pt-2 px-12   bg-[#94339d] border-2 border-[#edd400]  text-white mx-auto btn-hov rounded-xl "
