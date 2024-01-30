51225a79e05cb6b278f3662e3a35364e0761df73
noor alam <51040978+noorshanu@users.noreply.github.com>
2024-01-31 02:47:49 +0530

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -27,7 +27,7 @@ function Hero() {
             className=" mx-auto h-[2rem] sm:h-[40px]"
           />
         </div>
-        <div className=" sm:pt-0 pt-[53%]">
+        <div className=" sm:pt-0 pt-[50%]">
           <img
             src="images/tredtext.png"
             alt=""
