f90bbd929b42ff70afea7fe977a69e23b8d569a7
noor alam <51040978+noorshanu@users.noreply.github.com>
2024-01-30 19:54:31 +0530

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,9 +4,10 @@ import ClickStart from "../components/ClickStart";
 function Hero() {
   return (
     <section className=" relative hero-main h-[100vh]  ">
-      <img src="images/free.png" alt="" className=" absolute left-0 h-[200px]" />
-      <img src="images/winn.png" alt="" className=" absolute right-0 h-[200px]" />
+      
       <div className="container-wrapper relative z-50 flex items-center flex-col ">
+      <img src="images/free.png" alt="" className=" absolute left-0 sm:-left-[8%] h-[200px] sm:h-[320px] top-0 sm:top-[23%]" />
+      <img src="images/winn.png" alt="" className=" absolute right-0 sm:-right-[4%] h-[200px] sm:h-[320px] top-0 sm:top-[45%]" />
         <img src="images/chart.png" alt="" className=" absolute -z-[2] top-[45%] sm:top-[20%]" />
         <div className="py-3 sm:py-2">
           <img
