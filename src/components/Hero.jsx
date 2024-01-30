722f3a2889746818c7a311cdf8d72d54992cd93d
noor alam <51040978+noorshanu@users.noreply.github.com>
2024-01-31 02:33:25 +0530

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -57,7 +57,7 @@ function Hero() {
         </div>
 
         <div className="">
-          <h1 className="uppercase archo-black text-xl sm:text-3xl txt-s text-white text-center tracking-[-0.07em]">
+          <h1 className="uppercase archo-black text-xl sm:text-3xl txt-s text-white text-center tracking-[-0.11em]">
             {/* <img src="images/play.png" alt="" className=" mx-auto h-full sm:h-[50px]" /> */}
             <span className="text-[#edd400]">PLAY </span> FOR A SHARE OF
           </h1>
