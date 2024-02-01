c652fe790a81dbb254fe965ba5c651378a50cc78
noor alam <51040978+noorshanu@users.noreply.github.com>
2024-02-02 02:44:38 +0530

    Update ClickStart.jsx

--- src/components/ClickStart.jsx
+++ src/components/ClickStart.jsx
@@ -6,7 +6,7 @@ function ClickStart() {
         <div className=" mx-auto  justify-center mt-5 mb-2  flex">
             <a
               href="https://test.kix.digital/?utm_source=landingpage&utm_medium=website&utm_campaign=tradeathon&utm_content=wave3&utm_term=1millionkixtokens" 
-              className=" uppercase archo-black text-xl sm:text-5xl txt-s2 py-2 px-6   bg-[#fbe000] border-2 border-[#94339d]   mx-auto btn-hov rounded-xl  text-[#94339d]"
+              className=" uppercase archo-black text-4xl sm:text-5xl txt-s2 py-2 px-4 sm:px-6   bg-[#fbe000] border-2 border-[#94339d]   mx-auto btn-hov rounded-xl  text-[#94339d]"
             >
               {" "}
               Play <span className="text-[#000]">Now!</span>
